36a036010e03a23d64688ffb5d15e8fb1a0b3b76
windaaasaputri <windaekasaputri30@gmail.com>
2022-04-15 14:54:07 -0600

    mengubah kodingan location

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
       </div>
 
     <?php
-      header('Location: pages/dashboard.html');
+      header('Location: pages/dashboard.php');
       } else {
     ?>
       <div class="alert alert-danger" role="alert">
