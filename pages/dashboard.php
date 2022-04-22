2317b7065d9a5d927805bc9ee64d7bd293680eec
windaaasaputri <windaekasaputri30@gmail.com>
2022-04-22 13:49:45 -0600

    menambahkan link

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -70,19 +70,19 @@ if(isset($_POST['button_logout'])){
       <div class="position-sticky pt-3">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
+            <a class="nav-link active" aria-current="page" href="dashboard.php">
               <span data-feather="home"></span>
               Dashboard
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="karyawan.php">
               <span data-feather="users"></span>
               Data Karyawan
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="bagian.php">
               <span data-feather="layers"></span>
               Data Bagian
             </a>
